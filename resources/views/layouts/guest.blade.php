357797162b5bbad6f0582ee1429408329c79da5b
naqeebah014 <naqeebahkhan18@gmail.com>
2025-07-01 12:12:28 +0200

    Installations
    Installed breeze and npm

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,13 +15,16 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-indigo-50">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <div>
+                <a href="/">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </div>
 
-                <div class="text-center mb-4">
-    <h1 class="text-3xl font-bold text-indigo-600">TaskFlow Manager</h1>
-    <p class="text-sm text-gray-500">Organize your tasks effortlessly</p>
-        {{$slot}}
-</div>
-
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
+        </div>
     </body>
 </html>
