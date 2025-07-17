616a4c28bcfde23564741abfa71697b7db2dfa25
naqeebah014 <naqeebahkhan18@gmail.com>
2025-07-17 15:45:32 +0200

    More styling changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,11 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    
+
 
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen" style="background-color: #F4C2C2;">
+
             @include('layouts.navigation')
 
             <!-- Page Heading -->
